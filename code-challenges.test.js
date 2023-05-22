aef47fa44d76b78518c7e367507549a212eb7e48
RodCato <code@eliment.org>
2023-05-22 01:51:55 -0500

    Week 5 assesment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,24 +11,25 @@
 // Run the file with the following command: $ yarn jest
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+
 // Pseudocode
-// Input:
-// Output:
-// Function name:
-// Process:
+// Input: String 
+// Output: String with coded message
+// Function name: codedMessage
+// Process: replace letters in the input string using the .replace method to replace all instances of the letters "a", "e", "i", and "o" (both uppercase and lowercase using regex and the case insensitive property and global property /ig) in the input string with the numbers 4, 3, 1, and 0 and return the modified string 
 
 // a) Create a test with expect statements using the variables provided.
 
 describe("codedMessage", () => {
     it("takes in a string and returns a string with a coded message", () => {
         expect(codedMessage(secretCodeWord1)).toStrictEqual("L4ck4d41s1c4l")
-        // const secretCodeWord1 = "Lackadaisical"
+        const secretCodeWord1 = "Lackadaisical"
         // Expected output: "L4ck4d41s1c4l"
         expect(codedMessage(secretCodeWord2)).toStrictEqual("G0bbl3dyg00k")
-        // const secretCodeWord2 = "Gobbledygook"
+        const secretCodeWord2 = "Gobbledygook"
         // Expected output: "G0bbl3dyg00k"
         expect(codedMessage(secretCodeWord3)).toStrictEqual("3cc3ntr1c")
-        // const secretCodeWord3 = "Eccentric"
+         const secretCodeWord3 = "Eccentric"
         // Expected output: "3cc3ntr1c"
     })
 })
@@ -41,11 +42,7 @@ describe("codedMessage", () => {
 
 //   ReferenceError: codedMessage is not defined
 
-
 // b) Create the function that makes the test pass.
- const secretCodeWord1 = "Lackadaisical"
- const secretCodeWord2 = "Gobbledygook"
- const secretCodeWord3 = "Eccentric"
 
  const codedMessage = (string) =>  {
  return string.replace(/a/ig, "4").replace(/e/ig, "3").replace(/i/ig, "1").replace(/o/ig, "0")
@@ -74,18 +71,16 @@ describe("codedMessage", () => {
 
 describe("wordsThatIncludes", () => {
     it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
-        // const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+         const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
         expect(wordsThatIncludes(fruitArray, filterLetterA)).toStrictEqual(["Mango", "Apricot", "Peach"])
-        // const filterLetterA = "a"
+        const filterLetterA = "a"
         // Expected output: ["Mango", "Apricot", "Peach"]
         expect(wordsThatIncludes(fruitArray, filterLetterE)).toStrictEqual(["Cherry", "Blueberry", "Peach"])
-        // const filterLetterE = "e"
+        const filterLetterE = "e"
         // Expected output: ["Cherry", "Blueberry", "Peach"]
     })
 })
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-const filterLetterA = "a"
-const filterLetterE = "e"
+
 // FAIL  ./code-challenges.test.js
 // codedMessage
 //   ✓ takes in a string and returns a string with a coded message (3 ms)
@@ -99,8 +94,8 @@ const filterLetterE = "e"
 
 // b) Create the function that makes the test pass.
 
-const wordsThatIncludes = (array, letter) => {
-    return array.filter(word => word.toLowerCase().includes(letter))
+const wordsThatIncludes = (array, filterLetter) => {
+    return array.filter(word => word.toLowerCase().includes(filterLetter))
 }
 
 // PASS  ./code-challenges.test.js
@@ -116,21 +111,27 @@ const wordsThatIncludes = (array, letter) => {
 // Ran all test suites.
 // ✨  Done in 0.63s.
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
-
+// 
 // Pseudocode
-// Input:
-// Output:
-// Function name:
-// Process:
-
-// a) Create a test with expect statements using the variable provided.
+// Input: array of 5 numbers
+// Output: boolean (true or false)
+// Function name: fullHouse
+// Process: take an array of cards as an argument and use .reduce method on the array to create an object called counts that contains the number of times each card appears in the array. The result of the counts variable will be used in  2 additional variables to return if the object values have 3 of a kind (hasThree)or 2 pairs(hasPair) and return true or false if the object has both using the logical AND operator.
 
 describe("fullHouse", () => {
-    it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+        it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
         expect(fullHouse(hand1)).toStrictEqual(true)
+        const hand1 = [5, 5, 5, 3, 3]
+        // Expected output: true
         expect(fullHouse(hand2)).toStrictEqual(false)
+        const hand2 = [5, 5, 3, 3, 4]
+        // Expected output: false
         expect(fullHouse(hand3)).toStrictEqual(false)
+        const hand3 = [5, 5, 5, 5, 4]
+        // Expected output: false
         expect(fullHouse(hand4)).toStrictEqual(true)
+        const hand4 = [7, 2, 7, 2, 7]
+        // Expected output: true
     })
 })
 
@@ -146,31 +147,22 @@ describe("fullHouse", () => {
 
 //   ReferenceError: fullHouse is not defined
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
 
 // b) Create the function that makes the test pass.
 
+// This problem took a long time to solve. I had many blockers and each blocker led to a ton of tabs opened of google searches. Eventually I landed on the .reduce method and this resource https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce that eventually led me to understand that I can make it create an object after the return on line 159
 const fullHouse = (handArr) => {
-    const counts = handArr.reduce((acc, card) => {
-      acc[card] = (acc[card] || 0) + 1
-      return acc
+    const counts = handArr.reduce((total, card) => {
+      total[card] = (total[card] || 0) + 1
+      return total
     },
-    {})
-    const hasThreeOfAKind = Object.values(counts).includes(3)
+    {})//<--this line creates the object that contains the number of times each card appears in the array which I use for the following variables 
+        // at this point in the code before I used the Object.values I used console.log(counts) and was able to see object values as such { '3': 2, '5': 3 } from this point i just had to figure out how to return the boolean value of the object which led to using the .includes method.
+    const hasThree = Object.values(counts).includes(3)
     const hasPair = Object.values(counts).includes(2)
-    return hasThreeOfAKind && hasPair
+    return hasThree && hasPair
   }
-    // console.log(fullHouse(hand1))
-    // console.log(fullHouse(hand2))
-    // console.log(fullHouse(hand3))
-    // console.log(fullHouse(hand4))
+   
     
 //     PASS  ./code-challenges.test.js
 //     codedMessage
--- interview-questions.md
+++ interview-questions.md
@@ -10,25 +10,25 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Your answer: Calling super in Ruby calls the parent class method in the child class
 
-Researched answer:
+Researched answer: Calling super in a class will invoke the same method name in the superclass of the current class. This means that if a subclass has a method with the same name as a method in the superclass, calling super within the subclass will execute the method in the superclass.
 
 2. What is a gem?
 
 Your answer: A gem is a package manager that is used to build applications in Ruby.
 
-Researched answer:
+Researched answer: A gem is a package or library of code that can be easily installed and used in a Ruby application, essentially they are collections of reusable code that can help developers save time and effort by providing pre-built functionality that can be easily integrated into their own projects.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A database that organizes data in rows and columns. Im not sure of other database types.
 
-Researched answer:
+Researched answer: Relational database is a type of database management system (DBMS) that organizes data into tables with predefined relationships between them. It uses Structured Query Language (SQL) to manipulate and retrieve data. other types of databases include the following: NoSQL databases, including document databases, key-value stores, graph databases, columnar databases, and in-memory databases, are non-relational database systems that offer flexible schema designs and alternative data models to SQL-based relational databases. Document databases store and retrieve data in a document format, ideal for hierarchical or nested data. Key-value stores use unique keys to associate data, simplifying basic retrieval but lacking advanced querying. Graph databases represent data as nodes, edges, and properties, optimizing handling of interconnected data and complex relationships. Columnar databases store data in columns for efficient querying of large datasets, while in-memory databases prioritize fast data access by storing data primarily in memory.
 
 4. What are primary keys? Why purpose does a primary key serve?
 
-Your answer: A primary key is a unique identifier for a particular row of data in a database.
+Your answer: A primary key is a unique identifier for a particular row of data in a database. It is used to identify data in the database.
 
-Researched answer:
+Researched answer:  A primary key is a unique identifier for each record in a database table. It serves the purpose of uniquely identifying each record and ensuring that there are no duplicate records in the table which is important for maintaining data integrity and for enabling efficient queries and updates to the data.
 
 5. What are the five HTTP verbs? What is each verb's corresponding CRUD action?
 
@@ -38,18 +38,23 @@ Read = Get
 Update = Put and Patch
 Delete = Delete
 
-Researched answer:
+Researched answer: 
+GET: is used to retrieve information or data from a resource. In CRUD terms, this corresponds to the "Read" operation.
+POST: is used to submit new information or data to a resource. In CRUD terms, this corresponds to the "Create" operation.
+PUT: is used to update existing information or data on a resource. In CRUD terms, this corresponds to the "Update" operation.
+DELETE: is used to remove information or data from a resource. In CRUD terms, this corresponds to the "Delete" operation.
+PATCH: is used to partially update existing information or data on a resource. This is a newer addition to the HTTP verbs and doesn't have a direct correspondence to a CRUD operation, but it is often used in place of PUT when only a portion of a resource needs to be updated.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: are rules and conditions that are applied to the data being stored in a model. These validations ensure that the data meets specific requirements or constraints before it is saved to the database. Model validations help maintain data integrity and enforce business rules by preventing the creation or modification of invalid or inconsistent records.
 
-2. RESTful routes:
+2. RESTful routes: RESTful routes are an attempt to bring the operations that can be applied to an object into HTTP requests.
 
-3. ERB:
+3. ERB: Embedded Ruby, a templating language used in Rails views to embed Ruby code. ERB allows you to embed Ruby code within HTML or other text-based templates.
 
-4. Params:
+4. Params: A hash that stores parameters passed from the user in the request. These parameters typically include form inputs, URL parameters, and other data sent from the client to the server.
 
-5. API:
+5. API: Application Programming Interface is a set of rules and protocols that allows different software applications to communicate and interact with each other. APIs define the methods and data formats that applications can use to request and exchange information.
